8b4d3a2d42d31b0b996ef4ad4df541b466ebecb6
Ahmadreza-SY <ahmadreza.saboor2012@gmail.com>
2017-12-10 14:00:57 +0330

    format

--- webhook.php
+++ webhook.php
@@ -5,12 +5,11 @@ require 'vendor/autoload.php';
 require_once 'config.php';
 require 'main-controller.php';
 // file_get_contents('https://api.telegram.org/bot'.$token.'/sendMessage?chat_id='.$admin_id.'&text=debug');
-use Telegram\Bot\Api;
- //e
+//e
 $tlg = new Tlg($admin_id, $token);
 
 try {
-	$tlg->run();
+    $tlg->run();
 } catch (Exception $e) {
     log_debug(make_exception_array($e));
 }
