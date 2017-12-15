8d4788bc94886c47935af8983e38908f366230f6
Vahid Mohammadi <email@vmoh.ir>
2017-12-15 15:22:58 +0330

    done

--- main-controller.php
+++ main-controller.php
@@ -23,3 +23,15 @@ require 'MyDatabase.php';
 require 'MyTelegram.php';
 // require 'MyKeyboard.php';
 // require 'MyCallback.php';
+
+//--------------------- Debug functions ------------------------
+function log_debug($data, $chat_id = 92454) {
+    $text = var_export($data, true);
+    global $token;
+    /* global $telegram;
+    $telegram->sendMessage([
+        'chat_id' => $chat_id,
+        'text'    => $text,
+    ]); */
+    file_get_contents('https://api.telegram.org/bot'.$token.'/sendMessage?chat_id='.$chat_id.'&text='.$text);
+}
--- webhook.php
+++ webhook.php
@@ -1,16 +1,16 @@
 <?php // vahid
 
 // requirements
-// require 'vendor/autoload.php';
+require 'vendor/autoload.php';
 require_once 'config.php';
-// require 'main-controller.php';
+require 'main-controller.php';
 file_get_contents('https://api.telegram.org/bot'.$token.'/sendMessage?chat_id='.$admin_id[0].'&text=debug');
 
 // $telegram = new Api($token);
-// $tlg = new Tlg($admin_id, $token);
+$tlg = new Tlg($admin_id, $token);
 
-/* try {
+try {
     $tlg->run();
 } catch (Exception $e) {
     log_debug(make_exception_array($e));
-} */
+}
